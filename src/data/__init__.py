f6c91684efa7c62ae78522777d906bda34b11a42
MohammadReza <mr.mohammadreza.ahmadian@gmail.com>
2026-09-04 23:59:18 +0330

    add data module

---
+++ src/data/__init__.py
@@ -0,0 +1,6 @@
+"""Dataset discovery and loading utilities."""
+
+from .loader import DatasetInspection, inspect_raw_dataset, load_dataset
+
+__all__ = ["DatasetInspection", "inspect_raw_dataset", "load_dataset"]
+
